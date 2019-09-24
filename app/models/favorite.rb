8495b70c0f98368a992a5474ce7f4db0ae85f05d
sasakura_870 <a_20100409@yahoo.co.jp>
2019-09-24 10:50:12 +0900

    update favoriteモデルにvalidation追加
    下書き、自分の記事にはいいねできないように実装

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,17 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  # validates_uniqueness_of :article_id, scope: :user_id
   validates :article_id, uniqueness: { scope: :user_id }
+  validate :cannot_favorite_yourself, :cannot_favorite_draft
+
+  private
+
+  def cannot_favorite_yourself
+    article = Article.find_by(id: article_id)
+    errors.add(:your_article, '自分の記事にいいねはできません') if article.user_id == user_id
+  end
+
+  def cannot_favorite_draft
+    errors.add(:draft, '下書きにいいねはできません') unless Article.find_by(id: article_id).posted?
+  end
 end
