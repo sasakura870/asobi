fe11a36beec724fd3dbc4479537488430182e56f
sasakura_870 <a_20100409@yahoo.co.jp>
2019-09-26 12:44:05 +0900

    update 仮登録時の確認画面の修正と本登録の失敗時の動作の修正
    本登録が失敗した場合はホーム画面へ遷移するよう修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,9 +1,14 @@
 class AccountActivationsController < ApplicationController
+
   def edit
     user = User.find_by(email: params[:email])
     if user&.authenticate_activation(params[:id]) && !user.activated?
+      user.register
       login user
       flash[:success] = '本登録が完了しました！'
+      redirect_to root_path
+    else
+      flash[:danger] = '無効なURLです'
       redirect_to root_path
     end
   end
