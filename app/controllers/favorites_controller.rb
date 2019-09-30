657337b7c92c13da4f8b7bb5d751b63efd6357ac
sasakura_870 <a_20100409@yahoo.co.jp>
2019-09-30 22:16:42 +0900

    remove favoriteコントローラーのfilter_only_other_users_articleを削除
    favoriteモデルのvalidationと役割が被っていたため

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,8 @@
 class FavoritesController < ApplicationController
-  # TODO フィルタとvalidationの役割が被っている？
-  before_action -> { filter_only_register || filter_only_other_users_article }
-  # before_action :filter_only_register
+  before_action :filter_only_register
 
   def create
+    @article = Article.find_by(id: params[:article_id])
     favorite = current_user.favorites.build(article_id: @article.id)
     if favorite.save
       flash[:success] = 'いいねしました'
@@ -14,20 +13,13 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find_by(id: params[:article_id])
     favorite = Favorite.find_by(user_id: current_user.id, article_id: @article.id)
     if favorite&.destroy
       flash[:success] = 'いいねを取り消しました'
       redirect_to article_path(@article)
     else
-      # flash[:danger] = 'いいねの取り消しに失敗しました'
       request_422
     end
-  end
-
-  private
-
-  def filter_only_other_users_article
-    @article = Article.find_by(id: params[:article_id])
-    request_422 if @article.nil? || current_user.articles.include?(@article)
   end
 end
