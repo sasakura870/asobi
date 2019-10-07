6e14c1a36d0eb3f4a230d759fd89e62b7bd86303
sasakura_870 <a_20100409@yahoo.co.jp>
2019-10-07 15:00:50 +0900

    update 記事カードのデザインの修正
    デザイン修正とアニメーションの追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-  def user_photo(user, size: 50)
+  def user_photo(user, size: 50, class_name: '')
     if user.photo.attached?
       image_tag user.photo, size: "#{size}x#{size}"
     else
-      image_pack_tag 'default_photo.png', size: "#{size}x#{size}"
+      image_pack_tag 'default_photo.png', size: "#{size}x#{size}", class: class_name
     end
   end
 end
