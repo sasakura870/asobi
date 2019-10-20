4f93744f20c36f8b8bb776adeb729198d914b245
sasakura_870 <a_20100409@yahoo.co.jp>
2019-10-20 11:51:54 +0900

    remove Articleモデルからpostedカラムの削除
    postedを使用しているArticleモデルの処理も一部変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_085740) do
+ActiveRecord::Schema.define(version: 2019_10_20_024428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_18_085740) do
     t.string "title", null: false
     t.string "overview"
     t.text "content"
-    t.boolean "posted", default: false, null: false
     t.string "thumbnail"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
