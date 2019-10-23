43b9731b4a416e2624f3c1ac47572b41096c7c2c
sasakura_870 <a_20100409@yahoo.co.jp>
2019-10-23 13:27:25 +0900

    update Tag, TagMapモデルのvalidationの追加
    Tagモデルの一意性が保たれるように修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   has_many :tag_map
   has_many :users, through: :tag_map, source: :taggable, source_type: 'User'
   has_many :articles, through: :tag_map, source: :taggable, source_type: 'Article'
+
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { maximum: 140 }
 end
--- app/models/tag_map.rb
+++ app/models/tag_map.rb
@@ -1,4 +1,6 @@
 class TagMap < ApplicationRecord
   belongs_to :taggable, polymorphic: true
   belongs_to :tag
+
+  validates :tag_id, uniqueness: { scope: %i[taggable_id taggable_type] }
 end
