6b33987bec4671af11a35e23303137c8bacd93df
sasakura_870 <a_20100409@yahoo.co.jp>
2019-10-29 15:49:40 +0900

    update seed.rbの修正
    内部でbulk insertを使用。タグの作成とArticleへの紐つけの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'naoyachan',
-             nick_name: 'なおや@開発',
-             email: 'a_20100409@yahoo.co.jp',
-             introduction: "開発用ユーザーです。\nテスト",
-             password: 'hogehoge',
-             password_confirmation: 'hogehoge',
-             status: :admin)
-
+user_list = []
+user_list << User.new(
+  name: 'naoyachan',
+  nick_name: 'なおや@開発',
+  email: 'a_20100409@yahoo.co.jp',
+  introduction: "開発用ユーザーです。\nテスト",
+  password: 'hogehoge',
+  password_confirmation: 'hogehoge',
+  status: :register
+)
 99.times do |n|
-  User.create!(name: "Faker_#{n}",
-               email: Faker::Internet.email,
-               introduction: Faker::Lorem.sentence(word_count: 5),
-               password: 'password',
-               password_confirmation: 'password',
-               status: :register)
+  user_list << User.new(
+    name: "Faker_#{n}",
+    email: Faker::Internet.email,
+    introduction: Faker::Lorem.sentence(word_count: 5),
+    password: 'password',
+    password_confirmation: 'password',
+    status: :register
+  )
 end
+User.import user_list
 
 users = User.order(:created_at).take(10)
-main_user = User.first
-5.times do |n|
-  users.each do |user|
-    article = user.articles.create!(title: "#{user.name}-#{n}",
-                                    overview: Faker::Lorem.sentence,
-                                    content: Faker::Lorem.sentence(word_count: 10),
-                                    status: :published)
-    next if user == main_user
+users.each do |user|
+  article_list = []
+  5.times do |n|
+    article_list << user.articles.new(
+      title: "#{user.name}-#{n}",
+      overview: Faker::Lorem.sentence,
+      content: Faker::Lorem.sentence(word_count: 10),
+      status: :published,
+      # bulk insertはafter createが実行されない
+      id_digest: Digest::MD5.hexdigest(n.to_s)
+    )
+  end
+  Article.import article_list
+end
 
-    main_user.favorites.create!(article_id: article.id) if n > 2
-    if n == 0 || n == 4
-      main_user.comments.create!(article_id: article.id,
-                                 content: Faker::Lorem.sentence(word_count: 5))
-    end
+tag_list = []
+20.times do |n|
+  tag_list << Tag.new(name: n.to_s)
+  tag_list << Tag.new(name: (100 - n).to_s)
+end
+Tag.import tag_list
+
+tag_maps = []
+Tag.find_each do |tag|
+  Article.find_each do |article|
+    tag_maps << article.tag_maps.build(tag_id: tag.id) if (article.id % tag.id).zero? && article.tags.count <= 10
   end
 end
+TagMap.import tag_maps
