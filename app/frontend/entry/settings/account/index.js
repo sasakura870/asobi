225c5af5f970f469e390859944c66c20f18b348b
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-05 17:08:50 +0900

    update パスワード変更画面のデザイン修正
    土台となるHTMLを記述。cssは未実装

--- app/frontend/entry/settings/account/index.js
+++ app/frontend/entry/settings/account/index.js
@@ -2,7 +2,7 @@ import RememberButton from "../../../js/components/remember_button";
 
 document.addEventListener('DOMContentLoaded', function () {
   new vue({
-    el: "#remember-form",
+    el: "#container",
     components: {
       "remember-button": RememberButton
     }
