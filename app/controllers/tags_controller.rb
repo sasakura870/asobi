72288fb53467ee06d2892b06eb875c26a85652c5
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-07 13:01:50 +0900

    update タグ詳細画面の作成
    タグのフォロー機能は未実装

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,9 @@
 class TagsController < ApplicationController
-  layout 'left_sidemenu'
+  layout 'left_sidebar'
 
   def show
     @tag = Tag.find_by(name: params[:id])
+    @articles = @tag.articles.published.recent.page(params[:page])
     request_404 if @tag.nil?
   end
 end
