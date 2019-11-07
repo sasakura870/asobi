f542e93dadaf7145b4f3e5dc56b9a2cd8c1a4da0
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-07 10:19:56 +0900

    update プロフィール編集画面のユーザーアイコン部分のデザイン変更
    とりあえずこれで実装。余力があればトリミング、ファイルボタンのcssもやりたい

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def user_photo(user, size: 50, class_name: '')
     if user.photo.attached?
-      image_tag user.photo, size: "#{size}x#{size}"
+      image_tag user.photo, size: "#{size}x#{size}", class: class_name
     else
       image_pack_tag 'default_photo.png', size: "#{size}x#{size}", class: class_name
     end
