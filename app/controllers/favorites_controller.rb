6e10ffcc0edc89c7310b666e17a66c75bcfeae26
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-11 09:46:24 +0900

    update favorites#destroyのService層の導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,9 +14,18 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find_by(id: params[:article_id])
-    favorite = current_user.favorites.find_by(user_id: current_user.id, article_id: @article.id)
-    if favorite&.destroy
+    # @article = Article.find_by(id: params[:article_id])
+    # favorite = current_user.favorites.find_by(user_id: current_user.id, article_id: @article.id)
+    # if favorite&.destroy
+    #   head :ok
+    # else
+    #   request_422
+    # end
+    handler = Favorites::DestroyHandler.new(
+      user: current_user,
+      article_id: params[:article_id]
+    )
+    if handler.run
       head :ok
     else
       request_422
