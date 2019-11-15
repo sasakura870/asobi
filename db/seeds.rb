c3665eff15c02c8e481b7e4795427e3ae7822048
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-15 15:06:22 +0900

    update seed.rbの修正
    記事のコンテンツ用のダミーデータを作成、呼び出し。いいねとコメントの作成。一部ユーザーにphotoを付与

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,67 +7,69 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user_list = []
-user_list << User.new(
-  name: 'naoyachan',
-  nick_name: 'なおや@開発',
-  email: 'a_20100409@yahoo.co.jp',
-  introduction: "開発用ユーザーです。\nテスト",
-  password: 'hogehoge',
-  password_confirmation: 'hogehoge',
-  status: :register
-)
-99.times do |n|
+80.times do |n|
   user_list << User.new(
-    name: "Faker_#{n}",
-    email: Faker::Internet.email,
+    name: n.zero? ? 'naoyachan' : "Faker_#{n}",
+    nick_name: n.odd? ? Faker::Name.name : '',
+    email: n.zero? ? 'a_20100409@yahoo.co.jp' : "faker#{n}@example.com",
     introduction: Faker::Lorem.sentence(word_count: 5),
-    password: 'password',
-    password_confirmation: 'password',
+    password: 'hogehoge',
+    password_confirmation: 'hogehoge',
     status: :register
   )
 end
 User.import user_list
+Faker::Config.locale = :en
+User.take(20).each do |user|
+  user.photo.attach(
+    io: OpenURI.open_uri(Faker::Avatar.image(size: '200x200', set: 'set4', bgset: 'bg1')),
+    filename: 'photo.png',
+    content_type: 'image/png'
+  )
+end
+Faker::Config.locale = :ja
 
-users = User.order(:created_at).take(10)
-create_counter = 0
-article_list = []
+users = User.take(40)
+dummy_content = ''
+File.open('spec/actiontext/dummy_data.html', 'r') do |f|
+  dummy_content = f.read
+end
 users.each do |user|
   5.times do |n|
-    create_counter += 1
-    article_list << user.articles.new(
+    params = {
       title: "#{user.name}-#{n}",
       overview: Faker::Lorem.sentence,
-      content: Faker::Lorem.sentence(word_count: 10),
-      status: :published,
-      # bulk insertはafter createが実行されない
-      id_digest: Digest::MD5.hexdigest(create_counter.to_s)
-    )
+      content: dummy_content,
+      status: :published
+    }
+    tag_list = []
+    5.times { tag_list << Faker::Sports::Basketball.team }
+    5.times { tag_list << Faker::Games::Pokemon.name }
+    Articles::CreateHandler.new(
+      user: user,
+      params: params,
+      tag_names: tag_list.join(',')
+    ).run
   end
 end
-Article.import article_list
 
-tag_list = []
-20.times do |n|
-  tag_list << Tag.new(name: n.to_s)
-  tag_list << Tag.new(name: (100 - n).to_s)
-end
-Tag.import tag_list
-
-tag_maps = []
-Tag.find_each do |tag|
-  Article.find_each do |article|
-    tag_maps << article.tag_maps.build(tag_id: tag.id) if (article.id % tag.id).zero? && article.tags.count <= 10
-  end
-end
-TagMap.import tag_maps
-
+favorite_list = []
 comment_list = []
 Article.find_each do |article|
   users.each do |user|
-    comment_list << article.comments.new(
+    check_num = article.id * user.id
+    if (check_num % 7) < 4
+      favorite_list << article.favorites.build(
+        user_id: user.id
+      )
+    end
+    next unless (check_num % 7) < 2 && check_num.odd?
+
+    comment_list << article.comments.build(
       user_id: user.id,
       content: Faker::Lorem.sentence(word_count: 5)
     )
   end
 end
+Favorite.import favorite_list
 Comment.import comment_list
