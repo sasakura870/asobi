0025739391edae23c997c32d01536bcebca2386d
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-18 12:33:13 +0900

    remove comment_form.vueの削除
    コメント投稿をajaxで処理しないため

--- app/frontend/entry/articles/show.js
+++ app/frontend/entry/articles/show.js
@@ -1,7 +1,6 @@
 import CircleFavoriteButton from '../../js/components/circle_favorite_button';
 import FollowButton from '../../js/components/follow_button';
 import DeleteCommentButton from '../../js/components/delete_comment_button';
-// import CommentForm from '../../js/components/comment_form'
 
 document.addEventListener('DOMContentLoaded', function () {
   new vue({
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
       'circle-favorite-button': CircleFavoriteButton,
       'delete-comment-button': DeleteCommentButton,
       'follow-button': FollowButton
-      // 'comment-form': CommentForm
     }
   });
 });
