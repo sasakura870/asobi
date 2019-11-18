07832d9709d2ba2451f1a76613fa1d3753c00bc5
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-18 17:56:58 +0900

    update タグ詳細画面のN+1問題の修正
    いいねとコメントの数の取得のみSQLがまだたくさん走っている

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,12 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by(name: params[:id])
-    @articles = @tag.articles.published.recent.page(params[:page])
+    @articles = @tag
+                .articles
+                .includes(:tags, user: { photo_attachment: :blob }, tag_maps: :tag)
+                .published
+                .recent
+                .page(params[:page])
     request_404 if @tag.nil?
   end
 end
