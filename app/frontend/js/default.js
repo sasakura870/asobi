f32710f945fd7321f809fb060543fa1eb0bb4276
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-20 00:04:57 +0900

    update ヘッダーのドロップダウンメニューのデザイン修正
    クリック時に開くように修正。デザインも変更

--- app/frontend/js/default.js
+++ app/frontend/js/default.js
@@ -4,13 +4,28 @@ import tippy from "tippy.js";
 document.addEventListener('DOMContentLoaded', function () {
   // ドロップダウンメニュー
   const dropdown = document.getElementsByClassName("js-dropdown");
-  console.log(dropdown);
+  const dropdownMenu = document.getElementsByClassName("js-dropdown-target");
   for (let index = 0; index < dropdown.length; index++) {
-    console.log(`index : ${index}`)
-    dropdown[index].addEventListener('click', function (event) {
-      console.log(event);
+    const target = dropdown[index].querySelector(".js-dropdown-target");
+    dropdown[index].addEventListener('click', function () {
+      if (target.className.includes("u-hide")) {
+        target.classList.remove("u-hide");
+      }
     });
   }
+  // ドロップダウンメニュー外をクリックでメニューを閉じる
+  document.body.addEventListener("click", function (event) {
+    for (let index = 0; index < dropdown.length; index++) {
+      if (dropdown[index].contains(event.srcElement)) {
+        continue;
+      }
+
+      const element = dropdownMenu[index];
+      if (!element.className.includes("u-hide")) {
+        element.classList.add("u-hide");
+      }
+    }
+  });
 
   // flash表示用toast
   const type = document.getElementById("toast_type");
