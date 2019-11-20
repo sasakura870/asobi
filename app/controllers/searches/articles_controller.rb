1ab8d6b555b5dcce678458175e55f86484cfc2a8
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-20 14:52:28 +0900

    update 検索結果画面のページのレイアウトの作成
    レイアウト内で検索結果の表示部分以外を担当する

--- app/controllers/searches/articles_controller.rb
+++ app/controllers/searches/articles_controller.rb
@@ -1,10 +1,7 @@
 module Searches
   class ArticlesController < Base
     def index
-      @q = searches_query
-      # @articles = Article
-      #             .includes(user: :photo_attachment)
-      #             .published.search_title(@q).recent.page(params[:page])
+
     end
   end
 end
--- app/controllers/searches/base.rb
+++ app/controllers/searches/base.rb
@@ -1,9 +1,11 @@
 module Searches
   class Base < ApplicationController
+    before_action :set_query
+
     private
 
-    def searches_query
-      params[:q]
+    def set_query
+      @q = params[:q]
     end
   end
 end
