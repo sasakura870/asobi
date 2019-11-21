5005021820d0adc62bd9aaacba6b6234e7a4a6fd
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-21 19:49:08 +0900

    add ガチャ用のコントローラーの作成
    処理未実装

---
+++ app/controllers/capsule_toys_controller.rb
@@ -0,0 +1,6 @@
+class CapsuleToysController < ApplicationController
+  def create
+    # 記事を取ってくる
+    # それ用に整形する
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ Rails.application.routes.draw do
 
   resources :resend_emails, only: :create
 
+  resources :capsule_toys, only: :create
+
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
