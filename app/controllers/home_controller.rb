0e0ecaf6fc5c39b69338e9d644fe23e8db2bde68
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-21 16:51:35 +0900

    update トップページ用のコンテンツをいくつか作成
    タイムライン、javascript未実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,12 @@
 class HomeController < ApplicationController
   def index
     # TODO: viewModel検討する？
-    @daily_articles = Article
+    @trend_articles = Article
                       .includes(user: :photo_attachment)
                       .published
                       .recent
-                      .take(10)
-    @weekly_articles = Article
-                       .includes(user: :photo_attachment)
-                       .take(3)
-    @recommended_articles = Article.includes(user: :photo_attachment)
-                                   .recent.take(5)
+                      .take(12)
+    @trend_users = User.order(id: :desc).take(6)
+    @trend_tags = Tag.order(id: :desc).take(10)
   end
 end
