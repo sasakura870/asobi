d915e236833504c61e49b6a53cfefa5966cc751b
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-22 15:49:39 +0900

    update トップページのN + 1問題の修正、各ページのデザインの修正
    Asobiガチャの処理用にボタンのクラスにjs-capsule-toyを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,15 @@ class HomeController < ApplicationController
   def index
     # TODO: viewModel検討する？
     @trend_articles = Article
-                      .includes(user: :photo_attachment)
+                      .includes(:tags, user: { photo_attachment: :blob }, tag_maps: :tag)
                       .published
                       .recent
                       .take(12)
-    @trend_users = User.register.order(id: :desc).take(6)
+    @trend_users = User
+                   .includes(photo_attachment: :blob)
+                   .register
+                   .order(id: :desc)
+                   .take(6)
     @trend_tags = Tag.order(id: :desc).take(10)
   end
 end
