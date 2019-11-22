1f94ff98d39c2096f325bcec97f8549d3133b90c
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-22 16:51:28 +0900

    fix タグ入力フォームが正しく表示されないバグの修正
    selectizeを読み込んでいなかったことが原因

--- app/frontend/js/selectize.js
+++ app/frontend/js/selectize.js
@@ -1,14 +1,18 @@
+require("selectize");
+
 document.addEventListener('DOMContentLoaded', function () {
-  $("#article_tag").selectize({
-    plugins: ['restore_on_backspace', 'remove_button'],
-    delimiter: ",",
-    persist: false,
-    maxItems: 5,
-    create: function (input) {
-      return {
-        value: input,
-        text: input
+  if ($("#article_tag")[0]) {
+    $("#article_tag").selectize({
+      plugins: ['restore_on_backspace', 'remove_button'],
+      delimiter: ",",
+      persist: false,
+      maxItems: 5,
+      create: function (input) {
+        return {
+          value: input,
+          text: input
+        }
       }
-    }
-  });
+    });
+  }
 });
