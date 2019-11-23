802cee8ea7c690e6a5deff99d32d16a1cc99e5cc
sasakura_870 <a_20100409@yahoo.co.jp>
2019-11-23 21:03:06 +0900

    fix 記事投稿画面のエディターの各種ボタンが使えないバグの修正
    カスタムHTMLのtrix-editorをVueがコンポーネントとして認識していたことが原因。とりあえず記事投稿画面ではVueを読み込まないことで対応

--- app/frontend/js/load_vue_components.js
+++ app/frontend/js/load_vue_components.js
@@ -1,3 +1,4 @@
+import CapsuleToyArticle from "./components/capsule_toy_article";
 import CircleFavoriteButton from "./components/circle_favorite_button";
 import DeleteCommentButton from "./components/delete_comment_button";
 import DeleteDraftButton from "./components/delete_draft_button";
@@ -5,16 +6,25 @@ import FollowButton from "./components/follow_button";
 import FollowTagButton from "./components/follow_tag_button";
 import RememberButton from "./components/remember_button";
 
+vue.config.ignoredElements = [
+  'trix-toolbar',
+  'trix-editor',
+  'action-text-attachment'
+]
+
 document.addEventListener('DOMContentLoaded', function () {
-  new vue({
-    el: "#container",
-    components: {
-      CircleFavoriteButton,
-      DeleteCommentButton,
-      DeleteDraftButton,
-      FollowButton,
-      FollowTagButton,
-      RememberButton
-    }
-  })
+  if (document.getElementById("container") != null) {
+    new vue({
+      el: "#container",
+      components: {
+        // CapsuleToyArticle,
+        CircleFavoriteButton,
+        DeleteCommentButton,
+        DeleteDraftButton,
+        FollowButton,
+        FollowTagButton,
+        RememberButton
+      }
+    });
+  }
 });
